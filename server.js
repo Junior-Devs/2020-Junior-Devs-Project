1c49994f0f83600c4033387db0915c412ea018ed
Eduardo <jeduardo824@gmail.com>
2020-05-06 18:18:42 -0300

    change server to render frontend and add a sample endpoint

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/frontend/index.html'));
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+app.get('/api/getSomething', (req, res) => {
+    var list = ["Course 1", "Course 2", "Course 3"]
+    res.json(list)
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname + '/frontend/dist/index.html'))
 });
 
 app.listen(process.env.PORT || 4000, function(){
