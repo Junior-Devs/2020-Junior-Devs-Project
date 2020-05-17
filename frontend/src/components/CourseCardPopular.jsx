617b5e5e7dd46041754a3aeccec93fa63f7a9f61
Arkadiusz <arkadiusz@ostrowski.co>
2020-05-17 18:29:15 +0100

    simplify css naming, add placeholder, add render logic - add code which makes sure values below "0" won't render - add PropTypes to component - add defaultProps - update stories, add story which generates component with "1" as value

--- frontend/src/components/CourseCardPopular.jsx
+++ frontend/src/components/CourseCardPopular.jsx
@@ -1,28 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import cardPlaceholder from '../assets/images/placeholder/card-popular.png'
 import './CourseCardPopular.css';
 
-function CourseCardPopular({imgSrc, title, concepts, studentsEnrolled, reviews}) {
+function CourseCardPopular({title, concepts, studentsEnrolled, reviews}) {
   const listOfConcepts = concepts.reduce((first,second) => first.concat(", ", second));
-  const studentsStat = `${studentsEnrolled} ${studentsEnrolled === 1 ? "Student":"Students"}`;
-  const reviewsStat = `${reviews} ${reviews === 1 ? "Review":"Reviews"}`;
+  const studentsStat = `${studentsEnrolled < 0 ? 0 : studentsEnrolled} ${studentsEnrolled === 1 ? "Student":"Students"}`;
+  const reviewsStat = `${reviews < 0 ? 0 : reviews} ${reviews === 1 ? "Review":"Reviews"}`;
 
   return(
     <div className="card-popular">
-      <img className="card-popular--image" src={imgSrc} alt="course card popular image" />
-      <div className="card-popular--container">
-        <h3 className="card-popular--title">{title}</h3>
-        <h5 className="card-popular--concepts-title">Concepts Instructed</h5>
-        <p className="card-popular--concepts-content">{listOfConcepts}...</p>
-        <div className="card-popular--stat-container">
-          <a className="card-popular--stat" href="#">{studentsStat}</a>
-          <div className="card-popular--stat-br" />
-          <a className="card-popular--stat" href="#">{reviewsStat}</a>
-          <div className="card-popular--stat-br" />
-          <a className="card-popular--apply-now" href="#">Apply Now</a>
+      <img
+        className="course-image"
+        src={cardPlaceholder}
+        alt="course card popular image"
+      />
+      <div className="course-info">
+        <a href="#"><h3 className="course-title">{title}</h3></a>
+        <div className="concepts">
+          <h5 className="concepts--title">Concepts Instructed</h5>
+          <p className="concepts--list">{listOfConcepts}...</p>
+        </div>
+        <div className="course-stats">
+          <a className="course-stat" href="#">{studentsStat}</a>
+          <div className="course-stat-br" />
+          <a className="course-stat" href="#">{reviewsStat}</a>
+          <div className="course-stat-br" />
+          <a className="course-apply" href="#">Apply Now</a>
         </div>
       </div>
     </div>
   )
 }
+
+CourseCardPopular.propTypes = {
+  title: PropTypes.string.isRequired,
+  concepts: PropTypes.arrayOf(PropTypes.string).isRequired,
+  studentsEnrolled: PropTypes.number.isRequired,
+  reviews: PropTypes.number.isRequired,
+};
+
+CourseCardPopular.defaultProps = {
+  studentsEnrolled: 0,
+  reviews: 0
+};
 
 export default CourseCardPopular;
--- frontend/src/components/CourseCardPopular.stories.jsx
+++ frontend/src/components/CourseCardPopular.stories.jsx
@@ -9,11 +9,20 @@ export default {
 
 export const courseData = {
   id: '1',
-  imgSrc: 'https://bit.ly/3dThyIi',
   title: 'A journey to a "Zero Waste" Lifestyle',
   concepts: ['Concept1', 'Concept2'],
   studentsEnrolled: 350,
   reviews: 10,
 };
 
+export const courseSingularData = {
+  id: '1',
+  title: 'A course with just one review and student',
+  concepts: ['Concept1', 'Concept2'],
+  studentsEnrolled: 1,
+  reviews: 1,
+};
+
+
 export const Default = () => <CourseCardPopular {...courseData} />;
+export const SingularDigits = () => <CourseCardPopular {...courseSingularData} />;
