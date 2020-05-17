7cc962a8dad59dc228968270af9c3e1eb6eac0b2
Arnold Kibira <arnold.ril3y@gmail.com>
2020-05-17 16:23:31 +0300

    add propTypes to the LessonCard component

--- frontend/src/components/LessonCard/LessonCard.jsx
+++ frontend/src/components/LessonCard/LessonCard.jsx
@@ -1,24 +1,16 @@
 import React from "react";
-import styles from "./LessonCard.module.css";
+import "./LessonCard.module.css";
 import logo from "../../assets/pollution.png";
+import PropTypes from "prop-types";
 
-const LessonCard = (props) => {
+const LessonCard = ({ heading, title, description }) => {
   return (
     <div className="card">
       <div className="contentLeft">
         <div className="courseLogo">1</div>
-        <div className="courseTitle">Course 1</div>
-        <div className="courseHeading"> Title </div>
-        <p className="courseContent">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
-          quibusdam commodi explicabo, blanditiis autem perspiciatis
-          exercitationem et, eos quod amet dicta fugiat recusandae quis nam ea
-          doloremque delectus tenetur soluta. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Enim error quisquam suscipit voluptatem,
-          officia voluptates facere voluptatibus incidunt nemo reprehenderit
-          perspiciatis nihil ex mollitia magni maxime debitis consequuntur earum
-          inventore.
-        </p>
+        <div className="courseTitle">{heading}</div>
+        <div className="courseHeading"> {title} </div>
+        <p className="courseContent">{description}</p>
         <div className="buttonDiv">
           <button className="courseButton">CONTINUE</button>
         </div>
@@ -26,16 +18,20 @@ const LessonCard = (props) => {
         <div className="progress">
           <div className="progressBar"></div>
         </div>
-          <div className="progressPercent">45%</div>
+        <div className="progressPercent">45%</div>
         <div className="duration">45 minutes left</div>
       </div>
       <div className="contentRight">
         <img src={logo} alt="pollution.png" />
-        
       </div>
     </div>
   );
 };
 
-
 export default LessonCard;
+
+LessonCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
