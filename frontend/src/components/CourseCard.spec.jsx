78c84642100d94b5189072a11ac0a51aae20126f
Enzo Parodi <enzoparodi90@gmail.com>
2021-01-13 21:34:16 +0100

    Format all js, jsx and json files with prettier

--- frontend/src/components/CourseCard.spec.jsx
+++ frontend/src/components/CourseCard.spec.jsx
@@ -4,7 +4,7 @@ import CourseCard from "./CourseCard";
 
 test("displays name and description passed as prop correctly", () => {
   const testName = "Test Name";
-  const testDescription = "Test Description"
+  const testDescription = "Test Description";
   render(<CourseCard name={testName} description={testDescription} />);
 
   expect(screen.queryByText(testName, testDescription));
